93e395faccf6afae82b7c934924e9f6f5bd680cb
Maurici <hello@mauriciabad.com>
2019-05-23 19:36:07 +0200

    Removed App.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import issueList from './issues/issueList';
 import './index.css';
 import IssueLarge from './issues/IssueLarge';
-import NavBar from './NavBar';
+import NavBar from './layout/NavBar';
 
 ReactDOM.render(
   <div>
