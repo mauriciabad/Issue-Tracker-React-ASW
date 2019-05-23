2d044f1fb65e59a57d142578932d4ac2ddb78c0d
EC2 Default User <ec2-user@ip-172-31-28-6.us-east-2.compute.internal>
2019-05-23 16:10:00 +0000

    componente list issues

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import issues from './issues';
 import './index.css';
 
 ReactDOM.render(
+  <issues />,
   <App />,
   document.getElementById('root')
 );
