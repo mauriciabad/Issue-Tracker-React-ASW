fae51bac56aa6b0cf859d098c7dc73d90bb7698d
Maurici <hello@mauriciabad.com>
2019-05-25 16:20:36 +0200

    add container

--- src/index.js
+++ src/index.js
@@ -4,21 +4,23 @@ import IssueList from './issues/IssueList';
 import './index.css';
 import IssueLarge from './issues/IssueLarge';
 import NavBar from './layout/NavBar';
-import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Container from '@material-ui/core/Toolbar';
 
 ReactDOM.render(
   <div>
-
     <Router>
       <div>
         <NavBar />
 
-        <Switch>
-        <Route exact path="/issues" component={IssueList} />
-        <Route path="/issues/:id" component={IssueLarge} />
-        
-        <Route render={() => <Redirect to="/issues" />} />
-        </Switch>
+        <Container>
+          <Switch>
+            <Route exact path="/issues" component={IssueList} />
+            <Route path="/issues/:id" component={IssueLarge} />
+            
+            <Route render={() => <Redirect to="/issues" />} />
+          </Switch>
+        </Container>
       </div>
     </Router>
   </div>,
