e065e4aa039ea0f952de2b2a8d378f9cf6e9c2b5
Xavier <fontbx@gmail.com>
2019-05-27 18:59:35 +0200

    PUSH and DELETE comments

--- src/issues/Comentaris.js
+++ src/issues/Comentaris.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
 
 import './Comentaris.css'
+import moment from 'moment';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Comentaris extends Component {
   constructor() {
     super();
+    
     this.state = {
       comments: [],
+      newComment: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({newComment: event.target.value});
+  }
+
+  handleSubmit(event) {
+    
+    //TODO: Posar current user
+    let dataToSend = {
+      "text": this.state.newComment,
+      "issue_id": this.props.issue,
+      "user_id": "2"
+    }
+
+    fetch(`https://asw-issue.herokuapp.com/issues/${this.props.issue}/comments.json`, {
+      method: 'POST',
+      body: JSON.stringify(dataToSend),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then((response) => response.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => {
+      console.log('Success:', response);
+      alert('Comentari creat!',window.location.reload());
+    });
+
+    event.preventDefault();
+    
+  }
+
+  handleDelete(commentId) {
+    console.log("comentari a borra = ",this.props.issue , commentId)
+    fetch(`https://asw-issue.herokuapp.com/issues/${this.props.issue}/comments/${commentId}.json`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then((response) => response.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => {
+      console.log('Success:', response);
+      alert('Comentari eliminat!',window.location.reload());
+    });
+
+     
   }
 
   componentDidMount() {
@@ -23,22 +76,34 @@ class Comentaris extends Component {
   }
 
   render() {
-    const commentsList = this.state.comments.map(comment => {
+    const commentsList = this.state.comments.map((comment , i) => {
       return(
-        <div className="comentari">
-          <p><b>{comment._links.creator.name}</b></p>
+        <div className="comentari" key={i}>
+          <b>{comment._links.creator.name}</b>
           <p>{comment.text}</p>
-          <p>{comment.created_at}</p>
-        </div>
+          <p>{moment(new Date(comment.created_at)).fromNow()}</p>
+          <Button variant="contained" size="small" >Editar</Button>
+          <Button style={{marginLeft:'1em'}} variant="contained" size="small" 
+            onClick={() => {this.handleDelete(comment.id)}}>Eliminar</Button>
+        </div> 
       )
       
     })
 
     return (
-      <div> 
-        <span>------------------------------------------------------------------------------------</span>
+      <div className="border"> 
         <p>Comments ({this.state.comments.length})</p>
-        <p className="comentaris">{commentsList}</p>
+        <div className="comentaris">{commentsList}</div>
+        <form onSubmit={this.handleSubmit}>
+          <TextField className="full"
+            label="Comment"
+            placeholder="What would you like to say?"
+            margin="normal"
+            variant="outlined"
+            value={this.state.newComment}
+            onChange={this.handleChange}
+          ></TextField> 
+        </form>
       </div>
     )
   }
